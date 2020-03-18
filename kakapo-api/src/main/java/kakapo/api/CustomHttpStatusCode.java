e9251fac44170ef93b2ea531a6e43660f9738e24
Jeff Isaak <jisaak@protonmail.com>
2020-03-18 11:14:53 -0700

    Add custom HTTP code definition for no prekeys available.

--- kakapo-api/src/main/java/kakapo/api/CustomHttpStatusCode.java
+++ kakapo-api/src/main/java/kakapo/api/CustomHttpStatusCode.java
@@ -3,4 +3,5 @@ package kakapo.api;
 public class CustomHttpStatusCode {
     public static final int QUOTA_EXCEEDED = 442;
     public static final int INSUFFICIENT_KEY_LENGTH = 443;
+    public static final int NO_PREKEYS_AVAILABLE = 444;
 }
