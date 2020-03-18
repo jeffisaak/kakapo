0bc64a486e9988a3a1bdb3381f6a6e68eb8d3e02
Jeff Isaak <jisaak@protonmail.com>
2020-03-17 19:33:28 -0700

    Add api key to sign up response

--- kakapo-api/src/main/java/kakapo/api/response/SignUpResponse.java
+++ kakapo-api/src/main/java/kakapo/api/response/SignUpResponse.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class SignUpResponse {
 
-    public static SignUpResponse success(String guid) {
+    public static SignUpResponse success(String guid, String apiKey) {
         SignUpResponse result = new SignUpResponse();
         result.setGuid(guid);
+        result.setApiKey(apiKey);
         return result;
     }
 
     private String _guid;
+    private String _apiKey;
 
     @JsonProperty("guid")
     public String getGuid() {
@@ -19,5 +21,14 @@ public class SignUpResponse {
 
     public void setGuid(String guid) {
         _guid = guid;
+    }
+
+    @JsonProperty("apiKey")
+    public String getApiKey() {
+        return _apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        _apiKey = apiKey;
     }
 }
