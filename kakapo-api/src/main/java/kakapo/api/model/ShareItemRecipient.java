f168523d05458e3dbc647c5f4a01dff3b2b033e2
Jeff Isaak <jisaak@protonmail.com>
2020-03-19 15:43:18 -0700

    Replace encryption key field with signing key field

--- kakapo-api/src/main/java/kakapo/api/model/ShareItemRecipient.java
+++ kakapo-api/src/main/java/kakapo/api/model/ShareItemRecipient.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class ShareItemRecipient {
 
     private String _guid;
-    private String _encryptionPublicKey;
+    private String _signingPublicKey;
 
     @JsonProperty("guid")
     public String getGuid() {
@@ -16,12 +16,12 @@ public class ShareItemRecipient {
         _guid = guid;
     }
 
-    @JsonProperty("epk")
-    public String getEncryptionPublicKey() {
-        return _encryptionPublicKey;
+    @JsonProperty("signingPublicKey")
+    public String getSigningPublicKey() {
+        return _signingPublicKey;
     }
 
-    public void setEncryptionPublicKey(String encryptionPublicKey) {
-        _encryptionPublicKey = encryptionPublicKey;
+    public void setSigningPublicKey(String signingPublicKey) {
+        _signingPublicKey = signingPublicKey;
     }
 }
