6811cd29ddb56b22292b20f6e3a9b485876df150
Jeff Isaak <jisaak@protonmail.com>
2020-03-24 09:21:17 -0700

    Add hash to get backup version response

--- kakapo-api/src/main/java/kakapo/api/response/GetBackupVersionResponse.java
+++ kakapo-api/src/main/java/kakapo/api/response/GetBackupVersionResponse.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class GetBackupVersionResponse {
 
-    public static GetBackupVersionResponse success(Long backupVersion) {
+    public static GetBackupVersionResponse success(Long backupVersion, String backupHash) {
         GetBackupVersionResponse result = new GetBackupVersionResponse();
         result.setBackupVersion(backupVersion);
+        result.setBackupHash(backupHash);
         return result;
     }
 
     private Long _backupVersion;
+    private String _backupHash;
 
     @JsonProperty("backupVersion")
     public Long getBackupVersion() {
@@ -19,5 +21,14 @@ public class GetBackupVersionResponse {
 
     public void setBackupVersion(Long backupVersion) {
         _backupVersion = backupVersion;
+    }
+
+    @JsonProperty("backupHash")
+    public String getBackupHash() {
+        return _backupHash;
+    }
+
+    public void setBackupHash(String backupHash) {
+        _backupHash = backupHash;
     }
 }
